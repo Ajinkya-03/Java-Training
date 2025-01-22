ff0a3147a77f70db5b654685845683567259f74b
Ajinkya <patilajink2005@gmail.com>
2025-01-22 15:08:44 +0530

    exception prgm

---
+++ exeption.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+
+public class exeption {
+    public static void main(String[] args)
+        {
+            int a=10;
+            int b=0;
+            int c=a/b;
+            System.out.println(c);
+        }
+    }
+
+    // OUTPUT - Exception in thread "main" java.lang.ArithmeticException: / by zero
