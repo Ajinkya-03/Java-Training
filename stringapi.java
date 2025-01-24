704e5d7befc9c9a27c28fc9636ba0e561efed8e6
Ajinkya <patilajink2005@gmail.com>
2025-01-24 16:28:22 +0530

    initial commit

--- stringapi.java
+++ stringapi.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class stringapi {
     public static void main(String[] args)
     {
-        List<Integer> values=Arrays.asList(1,2,3,4,5,6,8,10);
+           List<Integer> values=Arrays.asList(1,2,3,4,5,6,8,10);
 
         values.stream().filter(n->n%2==0).forEach(System.out::println);
     }
